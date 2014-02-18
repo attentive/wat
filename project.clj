6b9b8441dba01c81f58097e8a9a6a2d9574c5b03
tflynch <tflynch@gmail.com>
2014-02-18 23:30:01 +0800

    Update version in project.clj and description

--- project.clj
+++ project.clj
@@ -1,9 +1,8 @@
-(defproject wat "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+(defproject wat "0.2.0"
+  :description "ID3 tag manipulation library"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :repositories [["java.net" "http://download.java.net/maven/2/"]]
-  :dependencies [[org.clojure/clojure "1.4.0"]
-                 [org/jaudiotagger "2.0.3"]
-                 [clj-diff "1.0.0-SNAPSHOT"]])
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [org/jaudiotagger "2.0.3"]])
--- test/wat/core_test.clj
+++ test/wat/core_test.clj
@@ -7,3 +7,12 @@
 (deftest read-test
   (testing "Reading :TRACK values from files."
     (is (= "Beethoven") (wat-val (read-audiofile "test/Beethoven - Fur Elise.mp3") :TRACK))))
+
+(deftest read-all-test
+  (testing "Reading all wat-vals from a file."
+    (is (= "Beethoven")
+        (let [vals (wat-vals (read-audiofile "test/Beethoven - Fur Elise.mp3"))]
+          (print vals) ; dump all the vals in the file to console
+          (:TRACK vals)))))
+
+(run-tests)
